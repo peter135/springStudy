f28001b45caf4834d154384cceb741ee6e247746
wangjian <wangjian@eazytec.com>
2022-08-06 20:52:22 +0800

    【update】

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -6,9 +6,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -44,9 +46,27 @@ public class StudentService {
        studentRepository.deleteById(studentId);
     }
 
+    @Transactional
+    public void updateStudent(Long studentId,
+                              String name,
+                              String email) {
+        Student student = studentRepository.findById(studentId).
+                orElseThrow(()-> new IllegalStateException("Student with id "+ studentId +" does not exists"));
 
-    public void updateStudent(Long studentId, String name, String email) {
+        if (name != null && name.length()>0 &&
+            !Objects.equals(student.getName(),name)){
+            student.setName(name);
+        }
 
-        
+        if (email != null && email.length()>0 &&
+                !Objects.equals(student.getEmail(),email)){
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+            if (studentOptional.isPresent()){
+                throw  new IllegalStateException("email taken");
+            }
+            student.setEmail(email);
+        }
+
+
     }
 }
