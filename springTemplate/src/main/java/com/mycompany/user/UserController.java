2bc71f7eb018a8fc73866a5b28c63867e99b05e1
wangjian <wangjian@eazytec.com>
2022-08-10 15:38:58 +0800

    【edit delete】

--- springTemplate/src/main/java/com/mycompany/user/UserController.java
+++ springTemplate/src/main/java/com/mycompany/user/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -24,6 +25,7 @@ public class UserController {
     @GetMapping("/users/new")
     public  String showNewForm(Model model) {
         model.addAttribute("user",new User());
+        model.addAttribute("pageTitle","add new user");
         return"user_form";
     }
 
@@ -32,6 +34,32 @@ public class UserController {
         service.save(user);
         ra.addFlashAttribute("message","The user has been saved successfully");
         return "redirect:/users";
+    }
+
+    @GetMapping("/users/edit/{id}")
+    public  String showEditForm(@PathVariable("id") Integer id, Model model,RedirectAttributes ra) {
+        try {
+            User user = service.get(id);
+            model.addAttribute("user",user);
+            model.addAttribute("pageTitle","edit user (id:" + id +")");
+            return "user_form";
+        } catch (UserNotFoundException e){
+            ra.addFlashAttribute("message", e.getMessage());
+            return "redirect:/users";
+        }
+
+    }
+
+    @GetMapping("/users/delete/{id}")
+    public  String deleteUser(@PathVariable("id") Integer id, RedirectAttributes ra) {
+        try {
+            service.delete(id);
+            ra.addFlashAttribute("message","the user id "+ id + "has been deleted");
+        } catch (UserNotFoundException e){
+            ra.addFlashAttribute("message", e.getMessage());
+        }
+        return "redirect:/users";
+
     }
 
 }
--- springTemplate/src/main/java/com/mycompany/user/UserService.java
+++ springTemplate/src/main/java/com/mycompany/user/UserService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -16,6 +17,25 @@ public class UserService {
 
     public void save(User user){
         repo.save(user);
+    }
+
+    public User get(Integer id) throws UserNotFoundException{
+       Optional<User> result = repo.findById(id);
+       if (result.isPresent()){
+            return result.get();
+       }
+       throw new UserNotFoundException("could not find the user with id" + id);
+    }
+
+    public void delete(Integer id) throws UserNotFoundException {
+
+        Long count = repo.countById(id);
+        if (count == null || count == 0){
+            throw new UserNotFoundException("could not find the user with id" + id);
+
+        }
+        repo.deleteById(id);
+
     }
 
 }
