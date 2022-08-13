f35081e6acf22891c5af12071ed82bfff659f8c1
wangjian <wangjian@eazytec.com>
2022-08-13 17:46:42 +0800

    【Jpa section1】

---
+++ DataJpa/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository){
+        return args -> {
+            Student maria = new Student("Maria",
+                    "Jones",
+                    "Maria.jones@email.com",
+                    21);
+            studentRepository.save(maria);
+        };
+    }
+
+}
